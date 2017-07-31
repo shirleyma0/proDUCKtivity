d9e187cd8e8d1eac8d9068953c3a557f4c57bade
akinam2001 <anikamahajan.usa@gmail.com>
2017-07-31 11:38:28 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -321,25 +321,25 @@
 
 !function() {
   var data = [
-    { eventName: 'Lunch Meeting w/ Mark', calendar: 'Work', color: 'orange' },
-    { eventName: 'Interview - Jr. Web Developer', calendar: 'Work', color: 'orange' },
-    { eventName: 'Demo New App to the Board', calendar: 'Work', color: 'orange' },
-    { eventName: 'Dinner w/ Marketing', calendar: 'Work', color: 'orange' },
+    { eventName: 'Read 20 pages of Macbeth', calendar: 'Homework', color: 'orange' },
+    { eventName: 'Finish Calc Homework', calendar: 'Homework', color: 'orange' },
+    { eventName: 'Start AP World notes', calendar: 'Homework', color: 'orange' },
+    { eventName: 'Do page 4 of Chem packet', calendar: 'Homework', color: 'orange' },
 
-    { eventName: 'Game vs Portalnd', calendar: 'Sports', color: 'blue' },
-    { eventName: 'Game vs Houston', calendar: 'Sports', color: 'blue' },
-    { eventName: 'Game vs Denver', calendar: 'Sports', color: 'blue' },
-    { eventName: 'Game vs San Degio', calendar: 'Sports', color: 'blue' },
+    { eventName: 'Hit the Gym', calendar: 'Fitness', color: 'blue' },
+    { eventName: 'Go for a walk', calendar: 'Fitness', color: 'blue' },
+    { eventName: 'Play volleyball', calendar: 'Fitness', color: 'blue' },
+    { eventName: 'Yoga', calendar: 'Fitness', color: 'blue' },
 
-    { eventName: 'School Play', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Parent/Teacher Conference', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Pick up from Soccer Practice', calendar: 'Kids', color: 'yellow' },
-    { eventName: 'Ice Cream Night', calendar: 'Kids', color: 'yellow' },
+    { eventName: 'Tidy the house', calendar: 'Home', color: 'yellow' },
+    { eventName: 'Do my laundry', calendar: 'Home', color: 'yellow' },
+    { eventName: 'Vacuum', calendar: 'Home', color: 'yellow' },
+    { eventName: 'Take out the trash', calendar: 'Home', color: 'yellow' },
 
-    { eventName: 'Free Tamale Night', calendar: 'Other', color: 'green' },
-    { eventName: 'Bowling Team', calendar: 'Other', color: 'green' },
-    { eventName: 'Teach Kids to Code', calendar: 'Other', color: 'green' },
-    { eventName: 'Startup Weekend', calendar: 'Other', color: 'green' }
+    { eventName: 'Meditate', calendar: 'Hobbies', color: 'green' },
+    { eventName: 'Paint or draw', calendar: 'Hobbies', color: 'green' },
+    { eventName: 'Write in my journal', calendar: 'Hobbies', color: 'green' },
+    { eventName: 'Listen to music', calendar: 'Hobbies', color: 'green' }
   ];
 
   
